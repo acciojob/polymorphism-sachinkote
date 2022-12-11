f173c63e7d4e8ac0a9c5fe813a5b182d3544c6c6
sacsakha <sacsakha@LIN51015113.corp.capgemini.com>
2022-12-11 15:30:55 +0530

    Polymorphism assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
+class product{
+	public int product(int x, int y) {
+		return x*y;
+	}
+	public int product(int x, int y, int z) {
+		return x*y*z;
+	}
+	public double product(double x, double y) {
+		return x*y;
+	}
+}
 public class Main {
 
+	
+	public static void main(String args[])
+	{
+		product p=new product();
+		int res1=p.product(10, 20);
+		System.out.println("res1: "+res1);
+		int res2=p.product(10, 20, 20);
+		System.out.println("res2: "+res2);
+		
+		double res3=p.product(10.0, 20.20);
+		System.out.println("res3: "+res3);
+	}
 }
